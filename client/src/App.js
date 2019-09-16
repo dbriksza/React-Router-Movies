b4008b4b87cf0ec5d89903acc300c9d1bd693817
dbriksza <dbriksza@gmail.com>
2019-09-16 16:32:39 -0400

    dried up code a bit

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,23 @@
-import React, { useState } from 'react';
-
-import SavedList from './Movies/SavedList';
+import React, { useState } from "react";
+import MovieList from "./Movies/MovieList";
+import SavedList from "./Movies/SavedList";
+import Movie from "./Movies/Movie";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
 const App = () => {
-  const [savedList, setSavedList] = useState( [] );
+  const [savedList, setSavedList] = useState([]);
 
   const addToSavedList = movie => {
-    setSavedList( [...savedList, movie] );
+    setSavedList([...savedList, movie]);
   };
 
   return (
     <div>
       <SavedList list={savedList} />
-      <div>Replace this Div with your Routes</div>
+      <div>
+        <Route exact path="/movies/:id" component={Movie} />
+        <Route exact path="/" component={MovieList} />
+      </div>
     </div>
   );
 };
